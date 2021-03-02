6817a3d22af92029918b7898daae3c984d6ea4a6
Robscha11 <64844264+Robscha11@users.noreply.github.com>
2021-03-02 17:23:02 +0100

    Add files via upload

---
+++ Argelaguet.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Argelaguet : MonoBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject rightHandController;
+
+    private GameObject selectionRayGO;
+    private LineRenderer selectionRayLR;
+
+    private GameObject feedbackRayGO;
+    private LineRenderer feedbackRayLR;
+
+    private GameObject rightRayIntersectionSphere;
+
+    private RaycastHit rightHit;
+    public LayerMask myLayerMask;
+
+
+    private void Awake()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+        rightHandController = GameObject.Find("RightHand Controller");
+
+        if (selectionRayGO == null)
+        {
+            selectionRayGO = new GameObject();
+            selectionRayGO.name = "Selection Ray";
+            selectionRayGO.transform.SetParent(rightHandController.transform, false);
+
+            selectionRayLR = selectionRayGO.AddComponent<LineRenderer>() as LineRenderer;
+            selectionRayLR.startWidth = 0.01f;
+            selectionRayLR.positionCount = 2; // two points (one line segment)
+            selectionRayLR.enabled = false;
+            selectionRayLR.material.color = Color.white;
+        }
+
+        if (feedbackRayGO == null)
+        {
+            feedbackRayGO = new GameObject();
+            feedbackRayGO.name = "Feedback Ray";
+            feedbackRayGO.transform.SetParent(rightHandController.transform, false);
+
+            feedbackRayLR = feedbackRayGO.AddComponent<LineRenderer>() as LineRenderer;
+            feedbackRayLR.startWidth = 0.01f;
+            feedbackRayLR.positionCount = 2; // two points (one line segment)
+            feedbackRayLR.enabled = false;
+        }
+
+        if (rightRayIntersectionSphere == null)
+        {
+            // geometry for intersection visualization
+            rightRayIntersectionSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //rightRayIntersectionSphere.transform.parent = this.gameObject.transform;
+            rightRayIntersectionSphere.name = "Right Ray Intersection Sphere";
+            rightRayIntersectionSphere.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+            rightRayIntersectionSphere.GetComponent<MeshRenderer>().material.color = Color.yellow;
+            rightRayIntersectionSphere.GetComponent<SphereCollider>().enabled = false; // disable for picking ?!
+            rightRayIntersectionSphere.SetActive(false); // hide
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ----------------- Argelaguet selection technique -----------------
+
+        // YOUR CODE - BEGIN
+        if (Physics.Raycast(mainCamera.transform.position, rightHandController.transform.TransformDirection(Vector3.forward), out rightHit, Mathf.Infinity, myLayerMask))
+        {
+            OnEnable();
+            Debug.Log("Did Hit");
+            // update ray visualization
+            selectionRayLR.SetPosition(0, mainCamera.transform.position);
+            selectionRayLR.SetPosition(1, rightHit.point);
+
+            feedbackRayLR.SetPosition(0, rightHandController.transform.position);
+            feedbackRayLR.SetPosition(1, rightHit.point);
+
+            // update intersection sphere visualization
+            rightRayIntersectionSphere.SetActive(true); // show
+            rightRayIntersectionSphere.transform.position = rightHit.point;
+        }
+        else // ray does not intersect with objects
+        {
+            OnEnable();
+            // update ray visualization
+            selectionRayLR.SetPosition(0, mainCamera.transform.position);
+            selectionRayLR.SetPosition(1, mainCamera.transform.position + rightHandController.transform.TransformDirection(Vector3.forward) * 1000);
+
+            feedbackRayLR.SetPosition(0, rightHandController.transform.position);
+            feedbackRayLR.SetPosition(1, rightHandController.transform.position + rightHandController.transform.TransformDirection(Vector3.forward) * 1000);
+
+
+            // update intersection sphere visualization
+            rightRayIntersectionSphere.SetActive(false); // hide
+        }
+
+        // visualize feedback ray
+        // YOUR CODE - END
+    }
+
+    private void OnDisable()
+    {
+        if (selectionRayLR != null) selectionRayLR.enabled = false;
+        if (feedbackRayLR != null) feedbackRayLR.enabled = false;
+        if (rightRayIntersectionSphere != null) rightRayIntersectionSphere.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if (selectionRayLR != null) selectionRayLR.enabled = true;
+        if (feedbackRayLR != null) feedbackRayLR.enabled = true;
+    }
+}
+
+/*comments about Argelaguet selection technique
+ * it's useful to point at objects that are behind the feedback ray of the hand
+ * it needs some time to get used to it how objects are selected
+ * the objects that you can see fully can be selected in every position and hence the advantage is that what you see you can select and you are not constrained to your hands view.
+ * no need to move your head to select different objects in the scene is also a nice feature that helps preventing neck tension
+ * with the visible feedback ray it's easier to control the selection ray in the scene
+ * 
+ * drawbacks:
+ * for us it's more intuitiv and faster to select with the hand pointer and not thinking about the distance between hand and controller to select sth.
+ * it's 
+ */ 
---
+++ DefaultRay.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class DefaultRay : MonoBehaviour
+{
+    private GameObject scene = null;
+    private GameObject rightHandController;
+    private XRController rightXRController;
+    private LineRenderer rightRayRenderer;
+    private GameObject rightRayIntersectionSphere;
+    private RaycastHit rightHit;
+    public LayerMask myLayerMask;
+    private GameObject selectedObject = null;
+
+    private bool gripButtonLF = false;
+
+    private Matrix4x4 oInit = Matrix4x4.identity;
+
+    void Awake()
+    {
+        scene = GameObject.Find("Scene");
+        rightHandController = GameObject.Find("RightHand Controller");
+
+        if (rightHandController != null) // guard
+        {
+            rightXRController = rightHandController.GetComponent<XRController>();
+
+            //rightRayRenderer = gameObject.AddComponent<LineRenderer>();
+
+            rightRayRenderer = rightHandController.GetComponent<LineRenderer>();
+            if (rightRayRenderer == null) rightRayRenderer = rightHandController.AddComponent<LineRenderer>() as LineRenderer;
+            //rightRayRenderer.name = "Right Ray Renderer";
+            rightRayRenderer.startWidth = 0.01f;
+            rightRayRenderer.positionCount = 2; // two points (one line segment)
+            rightRayRenderer.enabled = true;
+
+            // geometry for intersection visualization
+            rightRayIntersectionSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //rightRayIntersectionSphere.transform.parent = this.gameObject.transform;
+            rightRayIntersectionSphere.name = "Right Ray Intersection Sphere";
+            rightRayIntersectionSphere.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+            rightRayIntersectionSphere.GetComponent<MeshRenderer>().material.color = Color.yellow;
+            rightRayIntersectionSphere.GetComponent<SphereCollider>().enabled = false; // disable for picking ?!
+            rightRayIntersectionSphere.SetActive(false); // hide
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // ----------------- ray intersection stuff -----------------
+        // Does the ray intersect any objects
+        if (Physics.Raycast(rightHandController.transform.position, rightHandController.transform.TransformDirection(Vector3.forward), out rightHit, Mathf.Infinity, myLayerMask))
+        {
+            //Debug.Log("Did Hit");
+            // update ray visualization
+            rightRayRenderer.SetPosition(0, rightHandController.transform.position);
+            rightRayRenderer.SetPosition(1, rightHit.point);
+
+            // update intersection sphere visualization
+            rightRayIntersectionSphere.SetActive(true); // show
+            rightRayIntersectionSphere.transform.position = rightHit.point;
+        }
+        else // ray does not intersect with objects
+        {
+            // update ray visualization
+            rightRayRenderer.SetPosition(0, rightHandController.transform.position);
+            rightRayRenderer.SetPosition(1, rightHandController.transform.position + rightHandController.transform.TransformDirection(Vector3.forward) * 1000);
+
+            // update intersection sphere visualization
+            rightRayIntersectionSphere.SetActive(false); // hide
+        }
+
+        Dragging1(); // dragging version1: attach selected object to ray 
+        //Dragging2(); // dragging version2: move only virtually attached object dummy
+
+    }
+
+    private void Dragging1()
+    {
+        // mapping: grip button (middle finger)
+        bool gripButton = false;
+        rightXRController.inputDevice.TryGetFeatureValue(CommonUsages.gripButton, out gripButton);
+        //Debug.Log("middle finger rocker: " + gripButton);
+
+        if (gripButton != gripButtonLF) // state changed
+        {
+            if (gripButton) // up (false->true)
+            {
+                if (rightHit.collider != null && selectedObject == null)
+                {
+                    SelectObject(rightHit.collider.gameObject);
+                }
+            }
+            else // down (true->false)
+            {
+                if (selectedObject != null)
+                {
+                    DeselectObject();
+                }
+            }
+        }
+        gripButtonLF = gripButton;
+    }
+
+
+    private void SelectObject(GameObject go)
+    {
+        selectedObject = go;
+        selectedObject.transform.SetParent(rightHandController.transform, false); // worldPositionStays = true
+        Debug.Log("middle finger rocker: ");
+        // YOUR CODE - BEGIN
+        // compensate position and orientation offset of the hit game object and the rightHandController to prevent jumps      
+        Matrix4x4 o = Matrix4x4.TRS(selectedObject.transform.localPosition, selectedObject.transform.localRotation, selectedObject.transform.localScale);
+        Matrix4x4 d = Matrix4x4.TRS(rightHandController.transform.localPosition, rightHandController.transform.localRotation, rightHandController.transform.localScale);
+        Matrix4x4 n = Matrix4x4.TRS(rightHandController.transform.parent.localPosition, rightHandController.transform.parent.localRotation, rightHandController.transform.parent.localScale);
+        Matrix4x4 m = Matrix4x4.TRS(scene.transform.localPosition, scene.transform.localRotation, scene.transform.localScale);
+        Matrix4x4 x = Matrix4x4.TRS(rightHandController.transform.parent.parent.localPosition, rightHandController.transform.parent.parent.localRotation, rightHandController.transform.parent.parent.localScale);
+        Matrix4x4 mat = d.inverse * n.inverse * x.inverse * m * o;
+        SetTransformByMatrix(selectedObject, mat);
+        // YOUR CODE - END
+    }
+
+    private void DeselectObject()
+    {
+        selectedObject.transform.SetParent(scene.transform, false); // worldPositionStays = true
+        // YOUR CODE - BEGIN
+        // compensate for jumps of the selected object when reinserting to the scene-branch
+        Matrix4x4 o = Matrix4x4.TRS(selectedObject.transform.localPosition, selectedObject.transform.localRotation, selectedObject.transform.localScale);
+        Matrix4x4 d = Matrix4x4.TRS(rightHandController.transform.localPosition, rightHandController.transform.localRotation, rightHandController.transform.localScale);
+        Matrix4x4 n = Matrix4x4.TRS(rightHandController.transform.parent.localPosition, rightHandController.transform.parent.localRotation, rightHandController.transform.parent.localScale);
+        Matrix4x4 m = Matrix4x4.TRS(scene.transform.localPosition, scene.transform.localRotation, scene.transform.localScale);
+        Matrix4x4 x = Matrix4x4.TRS(rightHandController.transform.parent.parent.localPosition, rightHandController.transform.parent.parent.localRotation, rightHandController.transform.parent.parent.localScale);
+        Matrix4x4 mat = m.inverse * x * n * d * o;
+        SetTransformByMatrix(selectedObject, mat);
+        // YOUR CODE - END
+
+        selectedObject = null; //changed before in line 131
+    }
+
+    private void Dragging2()
+    {
+        // mapping: grip button (middle finger)
+        bool gripButton = false;
+        rightXRController.inputDevice.TryGetFeatureValue(CommonUsages.gripButton, out gripButton);
+        //Debug.Log("middle finger rocker: " + gripButton);
+
+        // YOUR CODE - BEGIN
+            if (gripButton) // up (false->true)
+            {
+                if (rightHit.collider != null && selectedObject == null)
+                {
+                    selectedObject = rightHit.collider.gameObject;
+                    Matrix4x4 o = Matrix4x4.TRS(selectedObject.gameObject.transform.localPosition, selectedObject.gameObject.transform.localRotation, selectedObject.gameObject.transform.localScale);
+                    Matrix4x4 d = Matrix4x4.TRS(rightHandController.transform.localPosition, rightHandController.transform.localRotation, rightHandController.transform.localScale);
+                    Matrix4x4 n = Matrix4x4.TRS(rightHandController.transform.parent.localPosition, rightHandController.transform.parent.localRotation, rightHandController.transform.parent.localScale);
+                    Matrix4x4 m = Matrix4x4.TRS(scene.transform.localPosition, scene.transform.localRotation, scene.transform.localScale);
+                    Matrix4x4 x = Matrix4x4.TRS(rightHandController.transform.parent.parent.localPosition, rightHandController.transform.parent.parent.localRotation, rightHandController.transform.parent.parent.localScale);
+                    oInit = d.inverse * n.inverse * x.inverse * m * o;
+                    
+                }
+                if (selectedObject != null)
+                {
+                    Matrix4x4 d = Matrix4x4.TRS(rightHandController.transform.localPosition, rightHandController.transform.localRotation, rightHandController.transform.localScale);
+                    Matrix4x4 n = Matrix4x4.TRS(rightHandController.transform.parent.localPosition, rightHandController.transform.parent.localRotation, rightHandController.transform.parent.localScale);
+                    Matrix4x4 m = Matrix4x4.TRS(scene.transform.localPosition, scene.transform.localRotation, scene.transform.localScale);
+                    Matrix4x4 x = Matrix4x4.TRS(rightHandController.transform.parent.parent.localPosition, rightHandController.transform.parent.parent.localRotation, rightHandController.transform.parent.parent.localScale);
+                    Matrix4x4 mat = m.inverse * x * n * d * oInit;
+                    SetTransformByMatrix(selectedObject, mat);
+                }
+                
+            }
+            else // down (true->false)
+            {
+                if (selectedObject != null)
+                {
+                    selectedObject = null;
+                    oInit = Matrix4x4.identity;
+                }
+            }
+    }
+
+    void SetTransformByMatrix(GameObject go, Matrix4x4 mat) // helper function
+    {
+        go.transform.localPosition = mat.GetColumn(3);
+        go.transform.localRotation = mat.rotation;
+        go.transform.localScale = mat.lossyScale;
+    }
+
+    void OnDisable()
+    {
+        rightRayRenderer.enabled = false;
+        rightRayIntersectionSphere.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        rightRayRenderer = rightHandController.GetComponent<LineRenderer>();
+        if (rightRayRenderer == null) rightRayRenderer = rightHandController.AddComponent<LineRenderer>() as LineRenderer;
+        rightRayRenderer.enabled = true;
+    }
+
+}
